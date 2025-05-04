59bf29bce839c858d640f7a154ed20201f5fd08c
saurabhsigma <saurabh962003@gmail.com>
2025-05-05 03:23:23 +0530

    m

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // Removed output: 'export' to enable server-side rendering
+    output: 'export',
 };
 
 module.exports = nextConfig;
