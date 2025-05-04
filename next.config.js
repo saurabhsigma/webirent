c60f9b742bab2f49f725c0ec327662a6d78025f1
saurabhsigma <saurabh962003@gmail.com>
2025-05-05 04:18:41 +0530

    Add image domain configuration for Next.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // Server-side rendering enabled for NextAuth
+    images: {
+        domains: [
+            'randomuser.me',
+            'images.unsplash.com',
+            'source.unsplash.com',
+            'picsum.photos',
+            'webirent.netlify.app',
+            'croo-rent.netlify.app'
+        ],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**',
+            },
+        ],
+    },
 };
 
 module.exports = nextConfig;
